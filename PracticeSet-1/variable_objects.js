8aee8e6467a03cea13c7f8c4caf93c3510033aab
adityakumar-09 <ashishadi2005@gmail.com>
2025-08-17 16:13:55 +0530

    Practice Set 1 Solved

---
+++ PracticeSet-1/variable_objects.js
@@ -0,0 +1,16 @@
+const a = "123"
+const b = "123"
+const c = a+b
+console.log(c) //adding two strings
+console.log(typeof(c)) //type of the output
+
+const students = {
+    ranjan : 25001,
+    ghanshyam : 25002,
+    shyam : 25003
+}
+
+console.log(students)
+// adding a new key into the object "students"
+students['adi'] = 25004
+console.log(students)
