37174485c7fb54f367bebc3051952b006cff3af2
adityakumar-09 <ashishadi2005@gmail.com>
2025-08-18 09:53:55 +0530

    practice set 4

---
+++ PracticeSet-4/escape_sequence.js
@@ -0,0 +1,2 @@
+console.log("adi\"tya")
+console.log("adi\"tya".length)
---
+++ PracticeSet-4/string_functions.js
@@ -0,0 +1,9 @@
+let name = "Aditya"
+console.log(name.includes("dit")) //search anywhere in the string
+console.log(name.includes("adi"))//case-sensitive
+console.log(name.startsWith("Adi")) // True if starts with the given string
+console.log(name.startsWith("adi"))
+console.log(name.endsWith("adi")) // True if ends with the given string
+
+console.log(name.toLowerCase()) // methos whichi is used to convert string into the lowecase
+console.log(name.toUpperCase()) // methos whichi is used to convert string into the UPPERCASE
